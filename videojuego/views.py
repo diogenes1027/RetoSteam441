6ab3080c9f49527ee468dd170e7a5b69d1a043d3
diogenes1027 <a01653251@itesm.mx>
2021-04-06 15:50:50 -0500

    Initial commit

---
+++ videojuego/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Reto
+
+# Create your views here.
+def index(request):
+    #return HttpResponse('<h1> Saludos desde Django</h1>')
+    return render(request,'index.html')
+
+def proceso(request):
+    nombre = request.POST['nombre']
+    nombre = nombre.upper()
+    return render(request, 'proceso.html',{'name':nombre})
+
+def datos(request):
+    jugadores = Reto.objects.all()
+    return render(request, 'datos.html', {'lista_jugadores':jugadores})
